240abc501762a9c3bc35a25e13825e3133b0d022
hanseld28 <hansel.donizete@gmail.com>
2020-07-17 17:15:45 -0300

    refactor: fix type erro on class name

---
+++ src/test/java/guru/springframework/GreetingTest.java
@@ -0,0 +1,23 @@
+package guru.springframework;
+
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
+
+
+public class GreetingTest {
+
+	@Test
+	void testHelloWorld() {
+
+	}
+	
+	@Test
+	void testHelloWorldWithName() {
+
+	}
+	
+
+}
