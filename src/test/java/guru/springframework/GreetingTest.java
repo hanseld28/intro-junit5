48a8a5b445a3f20d40a721a19a57cfbbea742432
hanseld28 <hansel.donizete@gmail.com>
2020-07-17 17:39:12 -0300

    add: setUp method with beforeEach annotation

--- src/test/java/guru/springframework/GreetingTest.java
+++ src/test/java/guru/springframework/GreetingTest.java
@@ -3,23 +3,26 @@ package guru.springframework;
 
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNotEquals;
+import org.junit.jupiter.api.BeforeEach;
 
 
 public class GreetingTest {
-
+	
+	private Greeting greeting;
+	
+	@BeforeEach
+	void setUp() {
+		System.out.println("In Before Each");
+		greeting = new Greeting();
+	}
+	
 	@Test
 	void testHelloWorld() {
-		Greeting greeting = new Greeting();
-		
 		System.out.println(greeting.helloWorld());
 	}
 	
 	@Test
 	void testHelloWorldWithName() {
-		Greeting greeting = new Greeting();
-		
 		System.out.println(greeting.helloWorld("Hansel"));
 	}
 	
