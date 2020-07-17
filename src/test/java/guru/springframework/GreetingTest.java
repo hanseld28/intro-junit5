c76cc5298e20b38c89b6c879c9f388148ca5048d
hanseld28 <hansel.donizete@gmail.com>
2020-07-17 17:52:02 -0300

    add: test cases to understand test lifecycle

--- src/test/java/guru/springframework/GreetingTest.java
+++ src/test/java/guru/springframework/GreetingTest.java
@@ -2,13 +2,20 @@ package guru.springframework;
 
 
 import org.junit.jupiter.api.Test;
-
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 
 
 public class GreetingTest {
 	
 	private Greeting greeting;
+	
+	@BeforeAll
+	public static void beforeClass() {
+		System.out.println("Before - I am only called Once!!!");
+	}
 	
 	@BeforeEach
 	void setUp() {
@@ -26,5 +33,18 @@ public class GreetingTest {
 		System.out.println(greeting.helloWorld("Hansel"));
 	}
 	
-
+	@Test
+	void testHelloWorldWithName2() {
+		System.out.println(greeting.helloWorld("Aléxia"));
+	}
+	
+	@AfterEach
+	void tearDown() {
+		System.out.println("In After Each...");
+	}
+	
+	@AfterAll
+	public static void afterClass() {
+		System.out.println("After - I am only called Once!!!");
+	}
 }
